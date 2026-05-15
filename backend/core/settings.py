481fdbb756c54ac5ab59f96b1786b2871c14555b
kumushsh1210 <kumushsh1210@gmail.com>
2026-05-15 19:01:31 +0500

    Update ALLOWED_HOSTS to allow all hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gawk$f_qs@uiq@vq6+1plxzr^oyr&on&!+v3a2o5$42u6xxc_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
